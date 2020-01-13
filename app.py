0fc676e736d80368ef605cbb7c20c8c077bdd735
Dhamodharancs17 <47274135+Dhamodharancs17@users.noreply.github.com>
2020-01-13 14:20:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ def hello():
 
 @app.route("/ask", methods=['POST'])
 def ask():
-  message = request.form['messageText'].encode('utf-8').lower()
+  message = request.form['messageText'].encode('utf-8').lower().decode()
   #kernel now ready for use
   while True:
     if message == "quit":
       #exit()
       bot_response = "Bye Bye. Ended Successfully.\n LOL just kidding :). I am running in server. So, I won't stop :D"
+    elif message == "hi":
+      bot_response = "Hello, Please start asking the questions"
     elif message.find("!content")!=-1:
       #Write in notepad
       context = message
@@ -27,7 +29,7 @@ def ask():
     elif (message.find("created")!=-1 and message.find("you")!=-1) or message.find("ananthaprakash")!=-1:
       bot_response = "I was created by Ananthaprakash"
     else:
-      bot_response = model_qa_ml([context],[message])      
+      bot_response = str(model_qa_ml([context],[message])      )
       return jsonify({'status':'OK','answer':bot_response})
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080, debug=True)
